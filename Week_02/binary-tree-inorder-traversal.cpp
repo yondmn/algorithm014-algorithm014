7379c5f9cb415f7ceeffb8adde501076ca679d11
杨明 <yangming@bigbear.local>
2020-08-17 09:30:18 +0800

    feat: binary tree inorder traversal

---
+++ Week_02/binary-tree-inorder-traversal.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> callstack;
+
+        TreeNode* current = root;
+        while (current != NULL || callstack.empty()) {
+            while (current != NULL) {
+                callstack.push(current);
+                current = current->left;
+            }
+            current = callstack.top();
+            callstack.pop();
+            res.push_back(current->val);
+            current = current->right;
+        }
+
+        return result;
+    }
+
+    vector<int> inorderTraversal1(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode *> call;
+        if (root != nullptr) call.push(root);
+        while(!call.empty()) {
+            TreeNode* t = call.top();
+            call.pop();
+            if (t != nullptr) {
+                if (t->right) call.push(t->right);
+                call.push(t);
+                call.push(nullptr);
+                if (t->left) call.push(t->left);
+            } else {
+                res.push_back(call.top()->val);
+                call.pop();
+            }
+        }
+
+        return res;
+    }
+};
